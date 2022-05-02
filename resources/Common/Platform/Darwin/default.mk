2e60b2d5dd8152b5c258878eaf5c59aa68ce6587
Branko Kolesarić <branko@kolesaric.me>
2022-05-02 15:53:18 +0200

    Add make help output

--- resources/Common/Platform/Darwin/default.mk
+++ resources/Common/Platform/Darwin/default.mk
@@ -1,1 +1,13 @@
-:help
+help: ## Prints this help
+	@grep --no-filename --extended-regexp '^ *[-a-zA-Z0-9_/]+ *:.*## ' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[45m%-20s\033[0m %s\n", $$1, $$2}'
+
+define permissions
+	setfacl -dRm          m:rwX  $(1)
+	setfacl -Rm           m:rwX  $(1)
+	setfacl -dRm u:`whoami`:rwX  $(1)
+	setfacl -Rm  u:`whoami`:rwX  $(1)
+	setfacl -dRm u:${RUNNER}:rwX $(1)
+	setfacl -Rm  u:${RUNNER}:rwX $(1)
+	setfacl -dRm u:root:rwX      $(1)
+	setfacl -Rm  u:root:rwX      $(1)
+endef
