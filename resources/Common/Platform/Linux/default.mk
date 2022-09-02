373816b5109081234ed11f409faa2b2b844792fb
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-09-02 13:45:50 +0200

    fix: define max CPUs available to use with Infection

--- resources/Common/Platform/Linux/default.mk
+++ resources/Common/Platform/Linux/default.mk
@@ -1,6 +1,8 @@
 help: ## Prints this help
 	@grep --no-filename --extended-regexp '^ *[-a-zA-Z0-9_/]+ *:.*## ' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[45m%-20s\033[0m %s\n", $$1, $$2}'
 
+OS_CPUS:=$(shell nproc)
+
 define permissions
 	setfacl -dRm          m:rwX  $(1)
 	setfacl -Rm           m:rwX  $(1)
