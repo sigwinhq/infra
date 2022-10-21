214cecf258a7abec66e984db57febf51a7a61f22
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-10-21 09:46:45 +0200

    feat: add Visual/default.mk

--- resources/Visual/common.mk
+++ resources/Visual/common.mk
@@ -1,5 +1,11 @@
+ifndef BASE_URL
+$(error BASE_URL must be defined before loading Visual/common.mk)
+endif
+
+PROJECT_ROOT := $(shell pwd)
+
 BACKSTOP_DOCKER_IMAGE ?= backstopjs/backstopjs:6.1.0
-BACKSTOP_DOCKER_COMMAND ?= docker run --init --interactive ${TTY} --shm-size 256MB --cap-add=SYS_ADMIN --rm --env BASE_URL=${BASE_URL} --user "$(shell id -u):$(shell id -g)" --tmpfs /tmp --volume "$(shell pwd):${APP_ROOT}" --workdir ${APP_ROOT} ${BACKSTOP_DOCKER_IMAGE} --config backstop.config.js
+BACKSTOP_DOCKER_COMMAND ?= docker run --init --interactive ${TTY} --shm-size 256MB --cap-add=SYS_ADMIN --rm --env PROJECT_ROOT=${PROJECT_ROOT} --env BASE_URL=${BASE_URL} --user "$(shell id -u):$(shell id -g)" --tmpfs /tmp --volume "${PROJECT_ROOT}:${PROJECT_ROOT}" --workdir "${PROJECT_ROOT}" ${BACKSTOP_DOCKER_IMAGE} --config backstop.config.js
 visual/test:
 	sh -c "${BACKSTOP_DOCKER_COMMAND} test"
 visual/reference: ## Generate visual testing references
