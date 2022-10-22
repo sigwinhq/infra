d0c47b488beb09b9ba6c40118c2099cdcf105426
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-10-22 15:11:02 +0200

    Monorepo support

---
+++ resources/PHP/library_monorepo.mk
@@ -0,0 +1,9 @@
+ifndef SIGWIN_INFRA_ROOT
+SIGWIN_INFRA_ROOT := $(dir $(abspath $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))))
+endif
+include ${SIGWIN_INFRA_ROOT}/PHP/common.mk
+include ${SIGWIN_INFRA_ROOT}/Monorepo/common.mk
+
+dist: cs all/composer/normalize all/analyze/phpstan all/analyze/psalm test ## Prepare the codebase for commit
+analyze: analyze/cs all/analyze/composer all/analyze/phpstan all/analyze/psalm ## Analyze the codebase
+test: all/test/infection ## Test the codebase
