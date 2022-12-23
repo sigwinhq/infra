0732c9bf4486c243cc84aa734fc0b440085714fb
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-12-23 18:53:00 +0100

    tests: Makefile test case

--- resources/Common/Platform/Windows/default.mk
+++ resources/Common/Platform/Windows/default.mk
@@ -1,5 +1,5 @@
 help: ## Prints this help
-	@grep --no-filename --extended-regexp '^ *[-a-zA-Z0-9_/]+ *:.*## ' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[45m%-20s\033[0m %s\n", $$1, $$2}'
+	@grep -h -E '^ *[-a-zA-Z0-9_/]+ *:.*## ' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[45m%-20s\033[0m %s\n", $$1, $$2}'
 
 OS_CPUS:=4
 
