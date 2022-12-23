5f49a0a9372b51203d799910f83c9950e49ba002
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2022-12-23 17:35:13 +0100

    tests: GH action

---
+++ resources/Common/Platform/Windows/default.mk
@@ -0,0 +1,20 @@
+help: ## Prints this help
+	@grep --no-filename --extended-regexp '^ *[-a-zA-Z0-9_/]+ *:.*## ' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[45m%-20s\033[0m %s\n", $$1, $$2}'
+
+OS_CPUS:=4
+
+define file_prefix
+	$(shell test -f ${2}${1} && echo -n ${2}${1} || echo ${1})
+endef
+
+# TODO: review
+define permissions
+	setfacl -dRm          m:rwX  $(1)
+	setfacl -Rm           m:rwX  $(1)
+	setfacl -dRm u:`whoami`:rwX  $(1)
+	setfacl -Rm  u:`whoami`:rwX  $(1)
+	setfacl -dRm u:${RUNNER}:rwX $(1)
+	setfacl -Rm  u:${RUNNER}:rwX $(1)
+	setfacl -dRm u:root:rwX      $(1)
+	setfacl -Rm  u:root:rwX      $(1)
+endef
