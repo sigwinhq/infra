fce40aa8abe3f85c2eea8d10d8460f92f1774fae
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-01-06 16:30:13 +0100

    fix: lighthouse requires RW volume

--- resources/Lighthouse/common.mk
+++ resources/Lighthouse/common.mk
@@ -1,5 +1,5 @@
 LIGHTHOUSE_DOCKER_IMAGE ?= cypress/browsers:node18.12.0-chrome107
-LIGHTHOUSE_DOCKER_COMMAND ?= docker run --init --interactive ${DOCKER_TTY} --rm --env HOME=/tmp ${DOCKER_USER} --volume "${DOCKER_CWD}:/project:ro" --workdir /project ${LIGHTHOUSE_DOCKER_IMAGE}
+LIGHTHOUSE_DOCKER_COMMAND ?= docker run --init --interactive ${DOCKER_TTY} --rm --env HOME=/tmp ${DOCKER_USER} --volume "${DOCKER_CWD}:/project" --workdir /project ${LIGHTHOUSE_DOCKER_IMAGE}
 
 analyze/lighthouse: ## Analyze built files using Lighthouse
 	${LIGHTHOUSE_DOCKER_COMMAND} npx lhci autorun --config=lighthouse.config.json
