731150f01952caa8c7cd54e875f231ca576c9cf6
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-11-30 11:21:15 +0100

    bump: Cypress Browsers, Lighthouse

--- resources/Lighthouse/common.mk
+++ resources/Lighthouse/common.mk
@@ -1,4 +1,4 @@
-LIGHTHOUSE_DOCKER_IMAGE ?= cypress/browsers:node-20.5.0-chrome-114.0.5735.133-1-ff-114.0.2-edge-114.0.1823.51-1
+LIGHTHOUSE_DOCKER_IMAGE ?= cypress/browsers:node-20.9.0-chrome-118.0.5993.88-1-ff-118.0.2-edge-118.0.2088.46-1
 LIGHTHOUSE_DOCKER_COMMAND ?= docker run --init --interactive ${DOCKER_TTY} --rm --env HOME=/tmp ${DOCKER_USER} --volume "${DOCKER_CWD}:/public" --workdir "/public" ${LIGHTHOUSE_DOCKER_IMAGE}
 
 analyze/lighthouse: ## Analyze built files using Lighthouse
