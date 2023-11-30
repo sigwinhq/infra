19965163d4fd2495a09da8d9b2575c99dc4ce86f
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-11-30 11:23:27 +0100

    bump: Cypress Browsers, Lighthouse (#150)

--- tests/functional/YASSG/CompatTest.php
+++ tests/functional/YASSG/CompatTest.php
@@ -120,7 +120,7 @@ final class CompatTest extends MakefileTestCase
     private static function generateDockerLighthouseExecutionPath(string $command): string
     {
         return sprintf(
-            'docker run --init --interactive  --rm --env HOME=/tmp %2$s --volume "$ROOT:/public" --workdir "/public" cypress/browsers:node-20.5.0-chrome-114.0.5735.133-1-ff-114.0.2-edge-114.0.1823.51-1 %1$s',
+            'docker run --init --interactive  --rm --env HOME=/tmp %2$s --volume "$ROOT:/public" --workdir "/public" cypress/browsers:node-20.9.0-chrome-118.0.5993.88-1-ff-118.0.2-edge-118.0.2088.46-1 %1$s',
             $command,
             self::generateDockerComposeExecutionUser()
         );
