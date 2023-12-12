f0b90f5678c1b1b10f372ad3e0515b1fdf3e8fac
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-12-12 10:18:23 +0100

    feat: start Compose on YASSG make start

--- resources/YASSG/common.mk
+++ resources/YASSG/common.mk
@@ -10,11 +10,13 @@ BASE_URL ?= file://localhost${APP_ROOT}/${BUILD_DIR}
 start: dev ## Start app in APP_ENV mode (defaults to "dev")
 start/dev: dev ## Start app in "dev" mode
 dev: clean
-	@make dev/assets dev/server -j2
+	@make dev/assets dev/server dev/compose -j3
 dev/server: vendor index.php
 	YASSG_SKIP_BUNDLES=${YASSG_SKIP_BUNDLES} symfony server:start --no-tls --document-root=. --port=${APP_PORT}
 dev/assets: node_modules
 	node_modules/.bin/encore dev-server
+dev/compose:
+	docker compose up
 index.php:
 	ln -s vendor/sigwin/yassg/web/index.php
 
