7570f15cf1b01760531e6ea94285c1f1d5f12cbd
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2024-01-19 15:26:12 +0100

    bump: all

--- resources/Lighthouse/common.mk
+++ resources/Lighthouse/common.mk
@@ -1,4 +1,4 @@
-LIGHTHOUSE_DOCKER_IMAGE ?= cypress/browsers:node-20.9.0-chrome-118.0.5993.88-1-ff-118.0.2-edge-118.0.2088.46-1
+LIGHTHOUSE_DOCKER_IMAGE ?= cypress/browsers:node-20.10.0-chrome-118.0.5993.88-1-ff-118.0.2-edge-118.0.2088.46-1
 LIGHTHOUSE_DOCKER_COMMAND ?= docker run --init --interactive ${DOCKER_TTY} --rm --env HOME=/tmp ${DOCKER_USER} --volume "${DOCKER_CWD}:/public" --workdir "/public" ${LIGHTHOUSE_DOCKER_IMAGE}
 
 analyze/lighthouse: ## Analyze built files using Lighthouse
